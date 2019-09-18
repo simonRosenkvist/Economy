959f6664a5811bc07e39dc97e518de92bc651166
simon <sr222qd@student.lnu.se>
2019-09-18 10:28:13 +0200

    main structure for color

--- src/experis/Main.java
+++ src/experis/Main.java
@@ -1,15 +1,36 @@
 package experis;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
+    static float prev;
+    static float current;
 
     public static void main(String[] args) {
 
         init();
+
     }
 
     public static void init() {
         APIHandler a = new APIHandler();
-        
+        current = a.call();
+
+        if (current < prev) {
+            //make red
+        }
+        else if ( current > prev) {
+            //make green
+        }
+        else {
+            //make white
+        }
+
+        prev = current;
+
         System.out.println(a.call());
+
+    }
+
     }
 }
