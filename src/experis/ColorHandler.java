2b23044f3b5995daf11fa2001d38914449f857ad
Pär <parso89@gmail.com>
2019-09-18 11:36:58 +0200

    color handler

--- src/experis/ColorHandler.java
+++ src/experis/ColorHandler.java
@@ -2,16 +2,23 @@ package experis;
 
 public class ColorHandler {
 
-        public void ColorHandler() {
+    private static final String ANSI_BRIGHT_RED    = "\u001B[91m";
+    private static final String ANSI_BRIGHT_GREEN  = "\u001B[92m";
+    private static final String[] FOREGROUNDS = { ANSI_BRIGHT_RED, ANSI_BRIGHT_GREEN };
 
-        }
 
-        public void makeRed(String input) {
-            System.out.println(input + " decrease");
-        }
+    public ColorHandler(){
 
-        public void makeGreen(String input) {
-            System.out.println(input + " increase");
-        }
+    }
+
+    public void makeRed(String message){
+        String setColor = FOREGROUNDS[0];
+        System.out.println(setColor + message);
+    }
+
+    public void makeGreen(String message){
+        String setColor = FOREGROUNDS[1];
+        System.out.println(setColor + message);
+    }
+
 }
-
