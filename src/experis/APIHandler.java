f69f2cc113c71287543e81aca931395780f6c85b
simon <sr222qd@student.lnu.se>
2019-09-18 09:09:28 +0200

    Main and APIHandler

---
+++ src/experis/APIHandler.java
@@ -0,0 +1,4 @@
+package experis;
+
+public class APIHandler {
+}
---
+++ src/experis/Main.java
@@ -0,0 +1,13 @@
+package experis;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        init();
+    }
+
+    public static void init() {
+
+    }
+}
